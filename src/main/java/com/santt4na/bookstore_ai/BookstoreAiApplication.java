6bdc46443586b6bc11b090795225c914d5747ab0
Santt4na <jhonnsantt4na@gmail.com>
2025-03-02 19:26:19 -0300

    Started Project

---
+++ src/main/java/com/santt4na/bookstore_ai/BookstoreAiApplication.java
@@ -0,0 +1,13 @@
+package com.santt4na.bookstore_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAiApplication.class, args);
+	}
+
+}
